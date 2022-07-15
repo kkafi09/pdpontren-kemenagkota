9394c875b29e044b8b27132d06053c550e6f6c2a
kkafi09 <afi.firda@gmail.com>
2022-07-15 15:08:05 +0700

    add - new feature visitor web

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BsWhatsapp } from "react-icons/bs";
+import countapi from "countapi-js";
+import axios from "axios";
 
 function HeaderSection() {
+  const [visitor, setVisitor] = useState(0);
+
+  useEffect(() => {
+    countapi
+      .hit(
+        `pdpontren-kemenagkota.vercel.app`,
+        `tanggal${new Date().getDate()}bulan${new Date().getMonth()}`
+      )
+      .then((response) => {
+        setVisitor(response.value);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-4xl lg:text-5xl font-bold text-green-600 text-center">
@@ -19,6 +34,13 @@ function HeaderSection() {
       >
         <BsWhatsapp /> <span className="px-2">Contact admin</span>
       </a>
+      <div className="mt-10 text-center w-3/4">
+        <p>
+          Pengunjung Website Hari ini {new Date().getDate()}/
+          {new Date().getMonth()}/{new Date().getFullYear()}
+        </p>
+        <h1 className="text-2xl mt-2">{visitor}</h1>
+      </div>
     </div>
   );
 }
